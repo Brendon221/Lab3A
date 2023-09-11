5ce9d3442d00cbbf17354ecf83a6cb7051169fbf
JSONfi1e <144738213+JSONfi1e@users.noreply.github.com>
2023-09-11 17:08:01 -0500

    Add files via upload

---
+++ Activity2code.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Name: JASON LAU
+#       ERIC LIU
+#
+# Section: 549
+# Assignment: LAB 3A
+# Date: 11 September 2023
+
+name1 = input("Enter name of person 1:")
+birthday1 = input(f'Enter the birthday of {name1}:')
+
+name2 = input("Enter name of person 1:")
+birthday2 = input(f'Enter the birthday of {name2}:')
+
+name3 = input("Enter name of person 1:")
+birthday3 = input(f'Enter the birthday of {name3}:')
+
+name4 = input("Enter name of person 1:")
+birthday4 = input(f'Enter the birthday of {name4}:')
+
+print(f"{name1}\t\t\t{birthday1}")
+print(f"{name2}\t\t\t{birthday2}")
+print(f"{name3}\t\t\t{birthday3}")
+print(f"{name4}\t\t\t{birthday4}")
